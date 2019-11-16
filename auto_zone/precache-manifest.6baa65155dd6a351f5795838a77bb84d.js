a84630b062d7e6d068ec179b4a20ccf0976b1b25
Santhosh kumar S <santhosh1989mpt@gmail.com>
2019-11-16 17:46:24 +0530

    Add files via upload

---
+++ auto_zone/precache-manifest.6baa65155dd6a351f5795838a77bb84d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4598107803df9217cb45e2a649ee247",
+    "url": "/auto_zone/index.html"
+  },
+  {
+    "revision": "9664f3802bc5115c3ce6",
+    "url": "/auto_zone/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "788bdbd9d93df167a9e1",
+    "url": "/auto_zone/static/css/main.1c0796d4.chunk.css"
+  },
+  {
+    "revision": "9664f3802bc5115c3ce6",
+    "url": "/auto_zone/static/js/2.8d7f82e6.chunk.js"
+  },
+  {
+    "revision": "788bdbd9d93df167a9e1",
+    "url": "/auto_zone/static/js/main.466d7e3a.chunk.js"
+  },
+  {
+    "revision": "d9b213f10d824b24e59c",
+    "url": "/auto_zone/static/js/runtime-main.c9760057.js"
+  },
+  {
+    "revision": "e3885d29843d6d8d466c53467c01202f",
+    "url": "/auto_zone/static/media/2.e3885d29.jpg"
+  },
+  {
+    "revision": "40459fb795ff6181f125fff1ed5fb1c0",
+    "url": "/auto_zone/static/media/3.40459fb7.jpg"
+  },
+  {
+    "revision": "57bfd7fd975243c20e6479d7bfbbef18",
+    "url": "/auto_zone/static/media/4.57bfd7fd.jpg"
+  },
+  {
+    "revision": "912f11fefdaea1d41f51941f1296e3a8",
+    "url": "/auto_zone/static/media/5.912f11fe.jfif"
+  },
+  {
+    "revision": "58da2f732546495ded453dbddc458020",
+    "url": "/auto_zone/static/media/6.58da2f73.jfif"
+  },
+  {
+    "revision": "6f51329ae70c2069bb93ed934b895d80",
+    "url": "/auto_zone/static/media/az-logo.6f51329a.svg"
+  }
+]);
